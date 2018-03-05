1ad34a98f1b51136c55b4cd4d7a3dcecfba06b0d
maikel <maikel.witlox@quovide.com>
2018-03-05 13:41:52 +0100

    dit werkt een beetje

--- UMLhuilen/src/MainPackage/Speelveld.java
+++ UMLhuilen/src/MainPackage/Speelveld.java
@@ -5,28 +5,18 @@ import java.awt.*;
 import javax.swing.*;  
   
 public class Speelveld{  
-JFrame f;  
+static JFrame f;  
 Speelveld(){  
     f=new JFrame();
     f.setSize(300, 400);
     f.setVisible(true);
     f.repaint();
-    Canvas C1 = new Canvas();
-    C1.setSize(50, 50);
-    C1.setBackground(Color.BLACK);
-    f.add(C1);
-    Canvas C2 = new Canvas();
-    C2.setLocation(50, 50);
-    C2.setBackground(Color.blue);
-    f.add(C2);
+    
 
-    f.setLayout(new GridLayout(3,3));  
-    //setting grid layout of 3 rows and 3 columns  
-  
-    f.setSize(300,300);  
-    f.setVisible(true);  
-}  
+}
 public static void main(String[] args) {  
-    new Speelveld();  
+    new Speelveld();
+    Muur muurtje = new Muur();
+    muurtje.Teken(f);
 }
 }  
--- UMLhuilen/src/MainPackage/Tegel.java
+++ UMLhuilen/src/MainPackage/Tegel.java
@@ -1,22 +1,31 @@
 package MainPackage;
+
 import java.awt.*;
 import javax.swing.*;
 
-public class Tegel extends JPanel{
+public class Tegel{
 	private int squareX = 50;
 	private int squareY = 50;
 	private int squareW = 20;
 	private int squareH = 20;
 	Coordinaat coor = new Coordinaat();
-	public void Teken(short rij, short column)
+	public void Teken(JFrame f)
 	{
-		coor.setColumn(column);
-		coor.setRij(rij);
-		
+		for(int i = 0; i < 9 ; i++){
+        Canvas Tegel = new Canvas();
+        Tegel.setBackground(Color.BLUE);
+        if(i == 2)
+        {
+        	Tegel.setBackground(Color.BLACK);
+        }
+		f.add(Tegel);
+		f.repaint();
+		}
+		  
+	    f.setLayout(new GridLayout(3,3));  
+	    //setting grid layout of 3 rows and 3 columns  
+	  
+	    f.setSize(300,300);  
+	    f.setVisible(true);  
 	}
-    protected void Teken(Graphics g) {
-        super.paintComponent(g); // do your superclass's painting routine first, and then paint on top of it.
-        g.setColor(Color.RED);
-        g.fillRect(squareX,squareY,squareW,squareH);
-    }
 }
